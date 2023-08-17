b4ffa614c5e673b01bd799507a760aea7169aa0f
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-17 05:27:40 +0100

    resume link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
-  let name = "World";
+	let name = 'World';
 </script>
 
 <header>
-  <nav class="main-nav">
-    <div class="brand-logo">
-      <a href="/">
-        <h1>&lt;h/&gt;</h1>
-      </a>
-    </div>
-    <div class="right">
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://docs.google.com/document/d/e/2PACX-1vSC1_JjJakdpurDkDhz_mRRdqBxcJJmYwagC-8jqqAZQQtAsxt8nYXkoskhelio8ong5YRX46svYHXd/pub"
-      >
-        <button class="btn btn-primary">RESUME</button>
-      </a>
-    </div>
-  </nav>
+	<nav class="main-nav">
+		<div class="brand-logo">
+			<a href="/">
+				<h1>&lt;h/&gt;</h1>
+			</a>
+		</div>
+		<div class="right">
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://docs.google.com/document/d/e/2PACX-1vSrcUD2WThZGZTSAN1bRfNMy1Zp5ZZEupuMY_xL6X0oc3A3dD5IuYWgq9ftqEVwx4si8zRpOJjCrfWd/pub"
+			>
+				<button class="btn btn-primary">RESUME</button>
+			</a>
+		</div>
+	</nav>
 </header>
 
 <style>
-  .brand-logo h1 {
-    color: #97b1a5;
-    font-size: 5rem;
-    vertical-align: middle;
-    text-align: center;
-    font-family: "Dancing Script", cursive, sans-serif;
-  }
+	.brand-logo h1 {
+		color: #97b1a5;
+		font-size: 5rem;
+		vertical-align: middle;
+		text-align: center;
+		font-family: 'Dancing Script', cursive, sans-serif;
+	}
 
-  .main-nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+	.main-nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
 </style>
