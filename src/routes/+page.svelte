293929fca8cf278f14034308c59888f495c0b9ae
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-17 12:10:09 +0100

    layout component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import 'global.css';
 	import Header from '../components/Header.svelte';
 	import Container from '../components/Container.svelte';
 </script>
 
-<div>
-	<Header />
-	<Container />
-</div>
+<Header />
+<Container />
