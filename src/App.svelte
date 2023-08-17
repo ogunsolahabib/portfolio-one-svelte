db3d3fe56e49fa0404d73097554033585fc4b66c
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-17 11:55:33 +0100

    remove footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import './global.css';
 	import Container from './components/Container.svelte';
-	import Footer from './components/Footer.svelte';
 	import Header from './components/Header.svelte';
 </script>
 
 <div>
 	<Header />
 	<Container />
-	<Footer />
 </div>
-
-<style lang="scss">
-</style>
