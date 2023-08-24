0daca393ecdeca5f25cb502992a0a53b1d319a57
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-24 03:18:09 +0100

    add footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Header from '../components/Header.svelte';
 	import Container from '../components/Container.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <Header />
 <Container />
+<Footer />
